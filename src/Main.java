ca6d094295f22e46604f794c6b4566db07366e1c
Kacper Kabak <kacperkabak456@Gmail.com>
2020-10-28 10:53:55 +0100

    pierwszy commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void  main(String[] args) {
+        int a = 12;
+        int b = 3;
+        double f = 4.5;
+        float af = 5.6f;
+        String s = " hello ";
+        boolean ab = true;
+        int array[] = {1, 2, 3};
+
+        final int c;
+        c = 19;
+        // c = 82;
+        System.out.println("Twoja suma wynosi:" +(a+b) );
+
+
+    }
+}
